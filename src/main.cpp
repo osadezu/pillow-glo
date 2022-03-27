0f3d4deb504d1b015edb433406a6a47c139ab30b
osadezu <osadezu@gmail.com>
2022-03-26 20:45:03 -0400

    Remove unused

--- src/main.cpp
+++ src/main.cpp
@@ -45,11 +45,6 @@ void ebbAndFlow(CRGB leds[], int numLeds)
     leds[i].g = noise;
   }
 
-  // long map(long x, long in_min, long in_max, long out_min, long out_max)
-  // {
-  //   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-  // }
-
   for (int in = 0; in < 256; in++)
   {
     Serial.print(in);
