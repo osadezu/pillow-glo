b7bbaa6ea4cd3398d6d39e65764b0e86331374ff
osadezu <osadezu@gmail.com>
2022-08-18 17:18:58 -0400

    Trailer Setup MVPish

--- src/pixels.cpp
+++ src/pixels.cpp
@@ -5,7 +5,7 @@
 #define DATA_PIN 18 // GPIO18 @ DevKit right-9
 
 /* Comment this line for final pixel setup */
-#define TESTGRID
+// #define TESTGRID
 
 #ifndef TESTGRID
 // Trailer setup values
@@ -375,6 +375,8 @@ namespace Pixels
 
   void ebbAndFlowLoops(uvLoop loop)
   {
+    uint16_t t = millis() / 2;
+    uint8_t scale = 50;
     uint8_t currExcitation = 255;
 
     // // Ease-in newly active loop
@@ -392,9 +394,6 @@ namespace Pixels
     {
       currExcitation = loop.cascadedExcitation;
     }
-
-    uint16_t t = millis() / 2;
-    uint8_t scale = 50;
 
     for (int i = 0; i < loop.ledCount; i++)
     {
